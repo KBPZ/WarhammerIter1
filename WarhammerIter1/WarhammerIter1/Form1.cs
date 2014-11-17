e5e1b9840fbc375b9e81daae0df73b4ce57d9c06
Urado <Samurai-94@mail.ru>
2014-11-17 17:35:07 +0300

    1

---
+++ WarhammerIter1/WarhammerIter1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarhammerIter1
+{
+    public partial class Form1 : Form
+    {
+        Game IsGame = new Game();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Green);
+            IsGame.IsMap.Paint(e,IsGame.PlayerNow());
+        }
+
+        private void tableLayoutPanel7_Click(object sender, EventArgs e)
+        {
+            if(IsGame.IsNowPfase(Pfase.Shoot))
+                IsGame.Shooting(IsGame.Target, 0, IsGame.Sourse);
+            tableLayoutPanel1.Invalidate();
+        }
+
+        private void tableLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel3_MouseClick(object sender, MouseEventArgs e)
+        {
+            IsGame.NextPfase();
+            tableLayoutPanel1.Invalidate();
+        }
+    }
+}
---
+++ WarhammerIter1/WarhammerIter1/Mission.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface intMission
+{
+    //private int []Score=new int[2]; 
+    void Begin(Map m);
+    void EndTurn(int p, Player pl);
+    void End();
+    void ShowScore();
+    void ShowName();
+}
+
+class Mission
+{
+}
+
+public  class EturnalWar2 : intMission
+{
+    public void Begin(Map m)
+    {
+
+    }
+    public void EndTurn(int p, Player pl)
+    {
+
+    }
+    public void End()
+    {
+
+    }
+    public void ShowName()
+    {
+
+    }
+    public void ShowScore()
+    {
+
+    }
+}
